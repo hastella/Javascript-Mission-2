7fbd6b24b7acbb047f092bdb7f07df8a3bde36cb
Stella_Ha <sangyeeh@gmail.com>
2022-11-22 18:31:01 +0900

    update

--- AnalogClock.js
+++ AnalogClock.js
@@ -1,5 +1,22 @@
-const AnalogClock = $container => {
-  // do something!
+const AnalogClock = ($container) => {
+  const deg = 6;
+  const hr = document.querySelector(".hand.hour");
+  const mn = document.querySelector(".hand.minute");
+  const sc = document.querySelector(".hand.second");
+
+  setInterval(() => {
+    // 현재 시간 가져오기
+    let day = new Date();
+
+    //시간을 단위로 추출
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+  });
 };
 
 export default AnalogClock;
